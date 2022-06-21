03a3986a8b5d217485c2d67a8457c4c06875513c
emanuelnav <emanuelnavarro.dev@gmail.com>
2022-06-21 10:58:13 -0300

    containers created and icons added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import '../styles/global.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Layout>
+      <Login/>
+    </Layout>
   )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -27,6 +28,21 @@ module.exports = {
             loader: 'html-loader'
           }
         ]
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|jpeg|webp)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/icons/[hash][ext]',
+        }
       }
     ]
   },
@@ -34,6 +50,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html'
-    })
-  ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+  }
 }
